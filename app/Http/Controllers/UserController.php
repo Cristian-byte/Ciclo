d10586a08e818c84d9a54befea853db821dd57d0
Cristian-byte <andygign40@gmail.com>
2021-02-16 17:25:34 -0600

    Creando Formulario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,16 +8,35 @@ class UserController extends Controller
 {
     public function index()
     {
+        $users = User::latest()->get();
 
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
 
+        $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8'],
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return back();
     }
 
-    public function destroy()
+    public function destroy(User $user)
     {
+        $user->delete();
 
+        return back();
     }
 }
